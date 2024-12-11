6e2e539c80daf726dcbc68ef470bd5b49d022cbe
Віталій Лабенський <vitaliy.laben@gmail.com>
2024-12-11 11:06:54 +0200

    Configure routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CarsTableComponent } from './cars-table/cars-table.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'list', component: CarsTableComponent },
+    { path: 'about', component: AboutComponent },
+];
